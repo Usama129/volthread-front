4e1d19145ecf630638d617aaeaa1073845b350b7
Usama129 <usama.humayun@ug.bilkent.edu.tr>
2021-01-12 03:31:23 +0300

    Added ping for musiqhq

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ app.use(express.static(__dirname + "/public"))
 function ping(){
     console.log("Pinging at " + new Date())
     http.get('http://employees-sb.herokuapp.com/ping')
+    http.get('http://musiqhq.herokuapp.com/ping') // another project of mine, needs to be kept alive
 }
 
 setInterval(ping, 20*60000) // ping heroku backend every 20 minutes to keep it from idling
